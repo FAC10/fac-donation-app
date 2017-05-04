26d5a70e5a424dbc392597f1b39dcd561443505f
Joey Scott <joannascott29@hotmail.co.uk>
2017-05-04 14:44:52 +0100

    Add payment-processor route Add payment-processor hbs file Add content to default.hbs file Register inert and vision in server.js Relates #27 #25 #23

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 const hapi = require('hapi');
+const inert = require('inert');
+const vision = require('vision');
+const handlebars = require('./handlebars');
 const routes = require('./routes');
 
 const server = new hapi.Server();
 
 
 server.connection({
-  // host: process.env.HOSTNAME || 'localhost',
   port: process.env.PORT || 4000,
 });
 
-server.route(routes);
+server.register([inert, vision], (err) => {
+  if (err) {
+    throw err;
+  }
+
+  server.views(handlebars);
+  server.route(routes);
+});
 
 module.exports = server;
