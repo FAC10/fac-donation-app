b88c4856eb0e9bc4a0c12d87c9381122f86add28
Joey Scott <joannascott29@hotmail.co.uk>
2017-05-05 15:52:45 +0100

    Add tests for charge route where there is no payload and incorrect payload Relates #34 #36

--- tests/src/test.js
+++ tests/src/test.js
@@ -1,4 +1,5 @@
 const test = require('tape');
+const stripe = require('stripe');
 const server = require('../../src/server.js');
 const stripeCharge = require('../../src/lib/charge.js');
 // const stripe = require('stripe')('sk_test_kpL5gpuCaeBdV72eIA3lZi1Z');
@@ -28,25 +29,56 @@ test('test that the payment-processor route returns a status code of 200', (t) =
   });
 });
 
-test('test that the charge route returns a status of 200', (t) => {
-  const options = {
-    method: 'POST',
-    url: '/charge',
-    payload: 'tok_1AG4BDDelOQugZzA6ymsuMQp',
-  };
+test('test that the charge route returns a status of 500 if there is no payload', (t) => {
 
-  stripeCharge((req) => {
+  const options = {
+      method: 'POST',
+      url: '/charge',
+    };
+
     server.inject(options, (res) => {
-      console.log(res);
-      t.equal(res.statusCode, 200, 'should return a status code of 200');
+      t.equal(res.statusCode, 500, 'should return a status code of 500');
+      t.end();
     });
-  });
-  t.end();
 });
 
+test('test charge route with incorrect payload', (t) => {
 
-// TFLCall(function (err, res) {
-//   var expected = {'message': 'oops, something has gone wrong'};
-//   t.deepEquals(err, expected, 'We recieved an error from TFL');
-//   t.end();
-// })
+  const options = {
+      method: 'POST',
+      url: '/charge',
+      payload: { stripeToken: 'thisIsAnIncorrectPayload' },
+    };
+
+    server.inject(options, (res) => {
+      // console.log(res);
+      t.equal(res.statusCode, 200, 'should return a status code of 200');
+      t.equal(res.payload, 'Sorry, there has been an error', 'should return error message');
+      t.end();
+    });
+});
+
+// test('test that the charge route returns a status of 200', (t) => {
+//
+//   stripe.createToken(card)
+//     .then(function(result) {
+//       console.log(result);
+//       stripeTokenHandler(result.token);
+//     })
+//     .catch(function(error) {
+//       var errorElement = document.getElementById('card-errors');
+//       errorElement.textContent = result.error.message;
+//     });
+//
+//   const options = {
+//       method: 'POST',
+//       url: '/charge',
+//       payload: { stripeToken: 'thisIsAnIncorrectPayload' },
+//     };
+//
+//     server.inject(options, (res) => {
+//       t.equal(res.statusCode, 200, 'should return a status code of 200');
+//       t.equal(res.payload, 'Sorry, there has been an error', 'should return error message');
+//       t.end();
+//     });
+// });
