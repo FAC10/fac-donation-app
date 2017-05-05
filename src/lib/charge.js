3f36a8b38779408c77ed3a4ec806c1b89e20ca76
bo-bok <bo-bok@users.noreply.github.com>
2017-05-05 14:29:36 +0100

    update charge request
    relates #34 #36

--- src/lib/charge.js
+++ src/lib/charge.js
@@ -3,16 +3,17 @@ const stripe = require('stripe')('sk_test_kpL5gpuCaeBdV72eIA3lZi1Z');
 function stripeCharge(req, reply) {
   const token = req.payload.stripeToken;
 
+// this function takes a object and a callback
   const charge = stripe.charges.create({
     amount: 1000,
     currency: 'gbp',
     description: 'example charge',
     source: token,
-  }, function (err, charge) {
+  }, (err, charge) => {
     // asynchronously called
     console.log(charge, 'this is the charge object');
-  },
-  );
+  }
+);
 }
 
 module.exports = stripeCharge;
