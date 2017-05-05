ff9f068f9da6022209af032ce1cd35377f5c8360
bo-bok <bo-bok@users.noreply.github.com>
2017-05-05 14:28:57 +0100

    add test for charge route
    relates #34 #36

--- tests/src/test.js
+++ tests/src/test.js
@@ -1,5 +1,8 @@
 const test = require('tape');
 const server = require('../../src/server.js');
+const stripeCharge = require('../../src/lib/charge.js');
+// const stripe = require('stripe')('sk_test_kpL5gpuCaeBdV72eIA3lZi1Z');
+
 
 test('test that the home route returns a status code of 200', (t) => {
   const options = {
@@ -24,3 +27,26 @@ test('test that the payment-processor route returns a status code of 200', (t) =
     t.end();
   });
 });
+
+test('test that the charge route returns a status of 200', (t) => {
+  const options = {
+    method: 'POST',
+    url: '/charge',
+    payload: 'tok_1AG4BDDelOQugZzA6ymsuMQp',
+  };
+
+  stripeCharge((err, reply) => {
+    server.inject(options, (res) => {
+      console.log(res);
+      t.equal(res.statusCode, 200, 'should return a status code of 200');
+      t.end();
+    });
+  });
+});
+
+
+// TFLCall(function (err, res) {
+//   var expected = {'message': 'oops, something has gone wrong'};
+//   t.deepEquals(err, expected, 'We recieved an error from TFL');
+//   t.end();
+// })
