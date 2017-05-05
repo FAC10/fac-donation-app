f507cac8af66e2fdf3b512eaa4461e91227c46d7
bo-bok <bo-bok@users.noreply.github.com>
2017-05-05 12:09:08 +0100

    create new oute which handles charges. Calls stripeCharge() which makes a call to Stripe API to handle payment
    relates #34 #36

---
+++ src/routes/charge.js
@@ -0,0 +1,11 @@
+const stripeCharge = require('../lib/charge.js');
+
+module.exports = {
+  method: 'POST',
+  path: '/charge',
+  handler: (req, reply) => {
+    // console.log(req.payload);
+    reply('you are on the charges page. give us ur dolla');
+    stripeCharge(req, reply);
+  },
+};
