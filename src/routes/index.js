70512f193a306b7a86dfe6a418ace9d0db6e8803
bo-bok <bo-bok@users.noreply.github.com>
2017-05-08 11:42:24 +0100

    export login-github and create-profile routes
    relates #41 #42

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,7 @@ module.exports = [
   require('./home'),
   require('./payment-processor'),
   require('./public'),
-  require('./charge')
+  require('./charge'),
+  require('./login-github'),
+  require('./create-profile')
 ];
