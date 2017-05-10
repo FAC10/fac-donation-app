1d98db89adf93be57008aa53211c1c1019da1fec
bo-bok <bo-bok@users.noreply.github.com>
2017-05-10 17:51:39 +0100

    insert fac relation function to route
    relates #60 #62 #65

--- src/routes/donation-first-time-user.js
+++ src/routes/donation-first-time-user.js
@@ -1,7 +1,15 @@
+const postdata = require('../database/postData.js');
+
 module.exports = {
   method: 'POST',
   path: '/donation-first-time-user',
   handler: (req, reply) => {
-    reply.view('donation-first-time-user');
+    postdata.insertFACRelation(req.payload, req.auth.credentials, (err, res) => {
+      if (err) {
+        console.log(err, 'there is an error');
+      } else {
+        reply.view('donation-first-time-user', { res });
+      }
+    })
   },
 };
