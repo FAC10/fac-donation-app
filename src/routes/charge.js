340a7bae3227328a73a52d58cf18ea598600af73
Joey Scott <joannascott29@hotmail.co.uk>
2017-05-10 22:42:22 +0100

    Add all new functions to /charge route Relates #25 #37 #60

--- src/routes/charge.js
+++ src/routes/charge.js
@@ -1,17 +1,56 @@
-const stripeCharge = require('../lib/charge.js');
+// const stripeCharge = require('../lib/charge.js');
+const stripeCustomer = require('../lib/create-customer.js');
+const doesCustomerExistInDB = require('../lib/doesCustomerExistInDB.js');
+const chargeRepeatCustomer = require('../lib/chargeRepeatCustomer');
+const saveStripeIdToDB = require('../lib/saveStripeIdToDB.js');
+const createNewCustomer = require('../lib/createNewCustomer.js')
 
 module.exports = {
   method: 'POST',
   path: '/charge',
   handler: (req, reply) => {
-    console.log('>>>>>>>>>>>>>>>>>>',req.payload);
-    stripeCharge(req, (err, res) => {
+    console.log('>>>>>>>>>>>>>>>>>>', req.payload);
+    // we get a req.payload which has a token
+    const stripeToken = req.payload.stripeToken;
+    const userCredentials = req.auth.credentials;
+
+    doesCustomerExistInDB(userCredentials, (err, result) => {
       if (err) {
-        console.log('ERRORRRRRR');
-        return reply.view('payment-failure');
+        console.log(err);
+        reply('Broken');
+      } else if (result === false) {
+        // create customer id and charge user
+        createNewCustomer(stripeToken, (err, result) => {
+          const stripe_id = result.id;
+          if (err) {
+            reply('unable to create new customer');
+          } else {
+            saveStripeIdToDB(stripe_id, userCredentials, (err, result) => {
+              if (err) {
+                reply('unable to save stripe id to db');
+              } else {
+                chargeRepeatCustomer(stripe_id, (err, result) => {
+                  if (err) {
+                    reply('unable to charge user');
+                  } else {
+                    reply('success1!!!');
+                  }
+                });
+              }
+            });
+          }
+        });
       } else {
-        console.log('YAYYYYYY');
-        return reply.view('payment-success');
+        const stripe_id = result.stripe_id;
+        chargeRepeatCustomer(stripe_id, (err, result) => {
+          if (err) {
+            reply('error, charge not gone through, they dont have any money.')
+          } else {
+            console.log(result);
+            reply('success!!!!')
+          }
+        });
+        // just charge user
       }
     });
   },
