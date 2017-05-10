e999409007362556125881e9568b4f8327a60bd0
bo-bok <bo-bok@users.noreply.github.com>
2017-05-10 17:40:28 +0100

    add frontend js file
    relates #5

---
+++ public/main.js
@@ -0,0 +1,9 @@
+// console.log('hello');
+//
+// document.querySelector('#test-button').addEventListener('click', function(e){
+//   // e.preventDefault();
+//   var testButton = document.querySelector('#test-button');
+//   console.log('i am a button');
+//   console.log(testButton);
+//
+// });
