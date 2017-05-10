677fb50f1752c9919d1d51bb3551cc5e690b06b4
bo-bok <bo-bok@users.noreply.github.com>
2017-05-10 17:51:54 +0100

    insert fac relation function to route
    relates #60 #62 #65

--- src/routes/profile-select-cohort.js
+++ src/routes/profile-select-cohort.js
@@ -1,8 +1,19 @@
+const postdata = require('../database/postData.js');
+
+
 module.exports = {
   method: 'POST',
   path: '/profile-select-cohort',
   handler: (req, reply) => {
     console.log(req.payload);
-    reply.view('profile-select-cohort');
+
+    postdata.insertFACRelation(req.payload, req.auth.credentials, (err, res) => {
+      if (err) {
+        console.log(err, 'there is an error');
+      } else {
+        reply.view('profile-select-cohort', { res });
+      }
+
+    });
   }
 }
