ec4f5150e6c111cc9836b274e1b0fb759c530dad
Joey Scott <joannascott29@hotmail.co.uk>
2017-05-11 22:01:29 +0100

    Parameter-ise all database queries and refactor code so that it is more consistent Use connect to refer to the database connection instead of db_connect Relates #68 #89

--- src/database/postData.js
+++ src/database/postData.js
@@ -1,13 +1,21 @@
-const db_connection = require('../../database/db_connection.js');
+const connect = require('../../database/db_connection.js');
 
 const postData = {};
 
 postData.insertGithubUser = (username, avatar_url, gitHubId, gitHubName, cb) => {
-  db_connection.query(`SELECT * FROM users WHERE github_username = '${username}' AND profile_picture = '${avatar_url}' AND github_id = '${gitHubId}' AND name = '${gitHubName}'`, (err, res) => {
+
+  const databaseQuery = `SELECT * FROM users WHERE github_username = $1 AND profile_picture = $2 AND github_id = $3 AND name = $4`;
+  const databaseParameters = [username, avatar_url, gitHubId, gitHubName];
+
+  connect.query(databaseQuery, databaseParameters, (err, res) => {
     if (err) {
       return cb(err);
     } else if (res.rows.length === 0) {
-        db_connection.query(`INSERT INTO users (github_username, profile_picture, github_id, name) VALUES ('${username}', '${avatar_url}', '${gitHubId}', '${gitHubName}')`, (err, res) => {
+
+        const databaseQuery = `INSERT INTO users (github_username, profile_picture, github_id, name) VALUES ($1, $2, $3, $4)`;
+        const databaseParameters = [username, avatar_url, gitHubId, gitHubName];
+
+        connect.query(databaseQuery, databaseParameters, (err, res) => {
           if (err) return cb(err);
           cb(null, res.rows);
         });
@@ -19,9 +27,10 @@ postData.insertGithubUser = (username, avatar_url, gitHubId, gitHubName, cb) => 
 
 postData.insertFACRelation = (reqPayload, credentials, callback) => {
 
-  const query = `UPDATE users SET fac_relation = '${reqPayload.relation}' WHERE github_username = '${credentials.username}'`;
+  const databaseQuery = `UPDATE users SET fac_relation = $1 WHERE github_username = $2`;
+  const databaseParameters = [reqPayload.relation, credentials.username]
 
-  db_connection.query(query, (err, res) => {
+  connect.query(databaseQuery, databaseParameters, (err, res) => {
     if (err) {
       console.log('threw an error', err)
       return callback(err, null);
@@ -34,11 +43,10 @@ postData.insertFACRelation = (reqPayload, credentials, callback) => {
 
 postData.insertFACCohort = (reqPayload, credentials, callback) => {
 
-  const query = `UPDATE users
-                 SET fac_cohort='${reqPayload.facCohort}', fac_location='${reqPayload.facLocation}'
-                 WHERE github_username = '${credentials.username}'`;
+  const databaseQuery = `UPDATE users SET fac_cohort = $1, fac_location = $2 WHERE github_username = $3`;
+  const databaseParameters = [reqPayload.facCohort, reqPayload.facLocation, credentials.username];
 
-  db_connection.query(query, (err, res) => {
+  connect.query(databaseQuery, databaseParameters, (err, res) => {
     if (err) {
       return callback(err, null);
     } else {
